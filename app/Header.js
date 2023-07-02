39571c77a8f3c510b2c49e61d0e3a7f714a20d44
DragonSenses <kendrickfrancisco@gmail.com>
2023-07-02 08:58:51 -0700

    Update Header.js

--- app/Header.js
+++ app/Header.js
@@ -2,6 +2,26 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <header className=''>
+      <h1 className=''>Header Name</h1>
+      <nav className=''>
+        <a href='#about-me' className=''>
+          <i className={`fa-solid fa-dragon `}></i>
+        </a>
+
+        <a href='#skills' className=''>
+          <i className={`fa-solid fa-list-check `}></i>
+        </a>
+
+        <a href='#projects' className=''>
+          <i className={`fa-solid fa-diagram-project `}></i>
+        </a>
+
+        <a href='#experience' className=''>
+          <i className={`fa-solid fa-book-open `}></i>
+        </a>
+
+      </nav>
+    </header>
   )
 }
