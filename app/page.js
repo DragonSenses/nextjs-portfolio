5abb7757eeaccf1ba9e6510999909a6d4d50b786
DragonSenses <kendrickfrancisco@gmail.com>
2023-07-02 09:40:36 -0700

    Import About component

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from 'next/image';
 import Header from './Header';
+import About from './About';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col flex-1 scroll-smooth">
       <Header />
+      <About />
     </main>
   )
 }
