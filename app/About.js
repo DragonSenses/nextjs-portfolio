01df43e2130ce41fadda7fdc712d6c232a9d75e1
DragonSenses <kendrickfrancisco@gmail.com>
2023-07-03 19:37:42 -0700

    Update About.js

--- app/About.js
+++ app/About.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import SectionIntro from './SectionIntro';
 
 export default function About() {
   return (
     <section id='about-me' className=''>
-      <div>
-        About Me
+      <div className=''>
+        <SectionIntro>ABOUT ME</SectionIntro>
+        <div className=''>This will contain content
+        
+        </div>
+
+        <div className=''>
+          <i className="fa-solid fa-dragon"></i>
+          <i className="fa-solid fa-dumbbell"></i>
+          <i className="fa-solid fa-book-open-reader"></i>
+          <i className="fa-solid fa-mug-hot"></i>
+        </div>
       </div>
     </section>
   )
