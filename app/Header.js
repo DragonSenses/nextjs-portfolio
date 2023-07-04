53a678941de194ed67f3cace1455d9d91a4642e1
DragonSenses <kendrickfrancisco@gmail.com>
2023-07-03 19:22:38 -0700

    Fix header text on larger screens

--- app/Header.js
+++ app/Header.js
@@ -4,8 +4,9 @@ export default function Header() {
   return (
     <header className='bg-gray-900 text-white min-h-screen flex sm:flex-col 
       justify-center items-center select-none gap-3.5'>
-      <h1 className='text-6xl text-center font-extrabold
-        [writing-mode:vertical-rl] [text-orientation:upright] [letter-spacing:-0.5ch]
+      <h1 className='text-6xl text-center font-extrabold sm:text-7xl sm:[text-align:unset]
+        sm:[writing-mode:unset] sm:[text-orientation:unset] sm:[letter-spacing:4px]
+        [writing-mode:vertical-rl] [text-orientation:upright] [letter-spacing:-0.25em]
         [text-shadow:_0.03em_0.03em_0_rgb(0_255_213),_0.06em_0.06em_0_rgb(154_192_18),_0.09em_0.09em_0_rgb(226_143_87),_0.11em_0.11em_0_rgb(161_24_127)]
         '>Header</h1>
       <nav className='flex flex-col sm:flex-row justify-center items-center
