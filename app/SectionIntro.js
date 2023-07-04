5bf4d2cfaa258c832a2a2119df444f20a484802e
DragonSenses <kendrickfrancisco@gmail.com>
2023-07-03 19:52:31 -0700

    Update SectionIntro.js

--- app/SectionIntro.js
+++ app/SectionIntro.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-export default function SectionIntro() {
+export default function SectionIntro(props) {
+  const { children, dark } = props;
   return (
-    <div>
-      SectionIntro
+    <div className=''>
+    <div className={''}></div>
+      <h1 className={''}>Section Intro{children}</h1>
+      <div className={''}></div>
     </div>
   )
 }
