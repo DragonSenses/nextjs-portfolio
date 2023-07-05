a83b60754c554f267765992e17a05d8de00522f6
DragonSenses <kendrickfrancisco@gmail.com>
2023-07-05 16:16:05 -0700

    Update Projects.js

--- app/Projects.js
+++ app/Projects.js
@@ -4,9 +4,14 @@ import SectionIntro from './SectionIntro';
 export default function Projects() {
   return (
     <section id='projects' className='sm:text-6xl'>
-      <SectionIntro>PROJECTS</SectionIntro>
-      <div className=''>
+      <div className='w-11/12 max-w-full flex items-center flex-col'>
+        <SectionIntro>PROJECTS</SectionIntro>
+        <div className='grid gap-1.5 [grid-template-columns:_repeat(1,_minmax(0,_1fr))]
+          sm:[grid-template-columns:_repeat(2,_minmax(0,_1fr))]
+          lg:[grid-template-columns:_repeat(3,_minmax(0,_1fr))] lg:gap-2.5'
+        >
 
+        </div>
       </div>
     </section>
   )
