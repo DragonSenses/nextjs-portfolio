86d941fdbc960a4c12ee1a643fae2fac5142156b
DragonSenses <kendrickfrancisco@gmail.com>
2023-07-04 19:16:53 -0700

    Create Skills.js

---
+++ app/Skills.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SectionIntro from './SectionIntro';
+
+export default function Skills() {
+  return (
+    <section id='skills' className='sm:text-6xl'>
+      <SectionIntro dark={true}>SKILLS</SectionIntro>
+
+      <div className=''>
+
+      </div>
+    </section>
+  )
+}
