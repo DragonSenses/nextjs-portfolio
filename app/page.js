51a4659a8d298e6a08c0a355c3cd2c60c3ac9c72
DragonSenses <kendrickfrancisco@gmail.com>
2023-07-04 19:23:39 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,16 @@
 import Image from 'next/image';
 import Header from './Header';
 import About from './About';
+import Skills from './Skills';
+import Projects from './Projects';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col flex-1 scroll-smooth">
       <Header />
       <About />
+      <Skills />
+      <Projects />
     </main>
   )
 }
