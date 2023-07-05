ac4b7c17bff0793390dbbb98ebe294069046256b
DragonSenses <kendrickfrancisco@gmail.com>
2023-07-05 16:49:49 -0700

    Update Projects.js

--- app/Projects.js
+++ app/Projects.js
@@ -10,6 +10,39 @@ export default function Projects() {
           sm:[grid-template-columns:_repeat(2,_minmax(0,_1fr))]
           lg:[grid-template-columns:_repeat(3,_minmax(0,_1fr))] lg:gap-2.5'
         >
+          <ImgOverlay
+            imgSrc={img1}
+            description={"first project is an ecommerce site"}
+            title={"PROJECT 1"}
+            projectLink={"https://google.com"}
+            githubLink={"https://google.com"}>
+            Paragraph describing project.
+            Built with: <strong>Next.js</strong> and <strong>Commerce.js</strong>
+          </ImgOverlay>
+
+          <ImgOverlay
+            imgSrc={img2}
+            description={"second project"}
+            title={"PROJECT 2"}
+            projectLink={"https://google.com"}
+            githubLink={"https://google.com"}>
+            Paragraph describing project.
+            Built with: <strong>Stripe.js</strong> and <strong>Zustand</strong>
+          </ImgOverlay>
+
+          <ImgOverlay
+            imgSrc={img3}
+            description={"third project is an ecommerce site"}
+            title={"PROJECT 3"}
+            projectLink={"https://google.com"}
+            githubLink={"https://google.com"}>
+            Paragraph describing project.
+            Built with: <strong>MongoDB</strong> and <strong>Node.js</strong>
+          </ImgOverlay>
+
+          <div className='vanilla_projects'>
+
+          </div>
 
         </div>
       </div>
