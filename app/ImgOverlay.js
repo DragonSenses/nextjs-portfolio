1018273a7d5cda04f1975602e4a1372797818188
DragonSenses <kendrickfrancisco@gmail.com>
2023-07-05 16:26:13 -0700

    Update ImgOverlay.js

--- app/ImgOverlay.js
+++ app/ImgOverlay.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
-export default function ImgOverlay() {
+export default function ImgOverlay(props) {
+  const { imgSrc, description, title, githubLink, projectLink, children } = props;
   return (
-    <div>ImgOverlay</div>
+    <div className='max-w-full object-contain relative duration-300'>
+      <Image 
+        className='max-w-full'
+        src={imgSrc}
+        alt={description}
+      />
+      <div className="overlay">
+        <h2 className="header"></h2>
+        <p className="para"></p>
+        <div className="links">
+          <Link></Link>
+          <Link></Link>
+        </div>
+      </div>
+    </div>
   )
 }
