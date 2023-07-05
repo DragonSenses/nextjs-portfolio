09f77269a7409b791d901dbadb11350ce225b13a
DragonSenses <kendrickfrancisco@gmail.com>
2023-07-04 19:19:40 -0700

    Create Projects.js

---
+++ app/Projects.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SectionIntro from './SectionIntro';
+
+export default function Projects() {
+  return (
+    <section id='projects' className='sm:text-6xl'>
+      <SectionIntro>PROJECTS</SectionIntro>
+      <div className=''>
+
+      </div>
+    </section>
+  )
+}
