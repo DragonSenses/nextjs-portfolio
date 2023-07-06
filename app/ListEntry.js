1fdce383c3860e4061c7bff8895d188e30d9b8a8
DragonSenses <kendrickfrancisco@gmail.com>
2023-07-06 06:58:16 -0700

    Update ListEntry.js

--- app/ListEntry.js
+++ app/ListEntry.js
@@ -4,7 +4,13 @@ export default function ListEntry(props) {
   const { title, date, children } = props;
   return (
     <div>
-      ListEntry
+      <div className='header'>
+        <h1 className='title'>{title}</h1>
+        <h1 className='date'>{date}</h1>
+      </div>
+      <ul className='ul'>
+        {children}
+      </ul>
     </div>
   )
 }
