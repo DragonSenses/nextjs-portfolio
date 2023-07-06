5f4c4b89be80f828c0ac3f5c30d6497e695c168b
DragonSenses <kendrickfrancisco@gmail.com>
2023-07-06 07:01:09 -0700

    Add styles to ListEntry

--- app/ListEntry.js
+++ app/ListEntry.js
@@ -4,11 +4,11 @@ export default function ListEntry(props) {
   const { title, date, children } = props;
   return (
     <div>
-      <div className='header'>
-        <h1 className='title'>{title}</h1>
-        <h1 className='date'>{date}</h1>
+      <div className='flex flex-col text-xs items-center pb-2'>
+        <h1 className='font-medium'>{title}</h1>
+        <h1 className='text-xs font-normal'>{date}</h1>
       </div>
-      <ul className='ul'>
+      <ul className='text-xs py-5 px-0'>
         {children}
       </ul>
     </div>
