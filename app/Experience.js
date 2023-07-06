63b475a15d4e89b9b89913b1e20a7058216f5cd3
DragonSenses <kendrickfrancisco@gmail.com>
2023-07-06 06:53:16 -0700

    Create Experience.js

---
+++ app/Experience.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Experience() {
+  return (
+    <section id="experience">
+      <div className=''>
+        <SectionIntro dark={true}>Experience</SectionIntro>
+      </div>
+    </section>
+  )
+}
