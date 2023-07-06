739669701936d51aa9399c6721c9236555a37f44
DragonSenses <kendrickfrancisco@gmail.com>
2023-07-06 07:05:12 -0700

    Responsive Design

--- app/ListEntry.js
+++ app/ListEntry.js
@@ -4,11 +4,12 @@ export default function ListEntry(props) {
   const { title, date, children } = props;
   return (
     <div>
-      <div className='flex flex-col text-xs items-center pb-2'>
-        <h1 className='font-medium'>{title}</h1>
-        <h1 className='text-xs font-normal'>{date}</h1>
+      <div className='flex flex-col text-xs items-center pb-2 
+      sm:flex-row sm:justify-between sm:items-center sm:pb-5'>
+        <h1 className='font-medium sm:text-2xl'>{title}</h1>
+        <h1 className='text-xs font-normal sm:text-base'>{date}</h1>
       </div>
-      <ul className='text-xs py-5 px-0'>
+      <ul className='text-xs py-5 px-0 sm:text-base'>
         {children}
       </ul>
     </div>
