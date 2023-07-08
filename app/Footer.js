b45adf6fb7ff29c6c6c4b38709712b091e73262d
DragonSenses <kendrickfrancisco@gmail.com>
2023-07-07 19:07:39 -0700

    Add content icon links

--- app/Footer.js
+++ app/Footer.js
@@ -6,7 +6,26 @@ export default function Footer() {
       "flex items-center justify-center gap-10 py-4 px-0 text-4xl sm:text-5xl bg-gray-900 text-white" : 
       "flex items-center justify-center gap-10 py-4 px-0 text-4xl sm:text-5xl bg-white text-gray-900"
     )}>
-      Footer
+      <a href="https://github.com/DragonSenses" 
+         className={(dark ? styles.iconDark : styles.icon)} 
+         target="_blank" rel="noreferrer"
+      >
+        <i className="fa-brands fa-github"></i>
+      </a>
+
+      <a href="https://google.com/" 
+         className={(dark ? styles.iconDark : styles.icon)} 
+         target="_blank" rel="noreferrer"
+      >
+        <i className="fa-solid fa-envelope"></i>
+      </a>
+
+      <a href="https://google.com/" 
+         className={(dark ? styles.iconDark : styles.icon)} 
+         target="_blank" rel="noreferrer"
+      >
+        <i className="fa-solid fa-address-card"></i>
+      </a>
     </footer>
   )
 }
