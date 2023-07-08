74e654b07baa2893ee33157851bfbf41f13ddf28
DragonSenses <kendrickfrancisco@gmail.com>
2023-07-08 16:51:09 -0700

    Update Projects.js
    - Properly serve up static assets
    - Use utility class for grid-cols
    - Formatting

--- app/Projects.js
+++ app/Projects.js
@@ -1,41 +1,42 @@
 import React from 'react';
 import SectionIntro from './SectionIntro';
+import ImgOverlay from './ImgOverlay';
 
 export default function Projects() {
   return (
     <section id='projects' className='sm:text-6xl'>
       <div className='w-11/12 max-w-full flex items-center flex-col'>
         <SectionIntro>PROJECTS</SectionIntro>
-        <div className='grid gap-1.5 [grid-template-columns:_repeat(1,_minmax(0,_1fr))]
-          sm:[grid-template-columns:_repeat(2,_minmax(0,_1fr))]
-          lg:[grid-template-columns:_repeat(3,_minmax(0,_1fr))] lg:gap-2.5'
-        >
+        <div className='grid gap-1.5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 lg:gap-2.5'>
           <ImgOverlay
-            imgSrc={img1}
+            imgSrc="/img1.jpg"
             description={"first project is an ecommerce site"}
             title={"PROJECT 1"}
             projectLink={"https://google.com"}
-            githubLink={"https://google.com"}>
+            githubLink={"https://google.com"}
+          >
             Paragraph describing project.
             Built with: <strong>Next.js</strong> and <strong>Commerce.js</strong>
           </ImgOverlay>
 
           <ImgOverlay
-            imgSrc={img2}
+            imgSrc="/img2.jpg"
             description={"second project"}
             title={"PROJECT 2"}
             projectLink={"https://google.com"}
-            githubLink={"https://google.com"}>
+            githubLink={"https://google.com"}
+          >
             Paragraph describing project.
             Built with: <strong>Stripe.js</strong> and <strong>Zustand</strong>
           </ImgOverlay>
 
           <ImgOverlay
-            imgSrc={img3}
+            imgSrc="/img3.jpg"
             description={"third project is an ecommerce site"}
             title={"PROJECT 3"}
             projectLink={"https://google.com"}
-            githubLink={"https://google.com"}>
+            githubLink={"https://google.com"}
+          >
             Paragraph describing project.
             Built with: <strong>MongoDB</strong> and <strong>Node.js</strong>
           </ImgOverlay>
