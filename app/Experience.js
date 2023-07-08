084ea0d3bc113aded93252f104a23b8443baed8b
DragonSenses <kendrickfrancisco@gmail.com>
2023-07-07 18:43:15 -0700

    Update Experience.js

--- app/Experience.js
+++ app/Experience.js
@@ -3,8 +3,12 @@ import React from 'react';
 export default function Experience() {
   return (
     <section id="experience">
-      <div className=''>
+      <div className='max-w-full w-11/12 flex items-center flex-col'>
         <SectionIntro dark={true}>Experience</SectionIntro>
+
+        <div className="flex flex-col w-full gap-8 py-0 px-5 sm:py-5 sm:px-12">
+
+        </div>
       </div>
     </section>
   )
