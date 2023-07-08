a49e7d744e6e1cd79875e54f9679a693af22d68e
DragonSenses <kendrickfrancisco@gmail.com>
2023-07-07 19:03:07 -0700

    Conditionally render dark or light footer

--- app/Footer.js
+++ app/Footer.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <div>
+    <footer className={(dark ? 
+      "flex items-center justify-center gap-10 py-4 px-0 text-4xl sm:text-5xl bg-gray-900 text-white" : 
+      "flex items-center justify-center gap-10 py-4 px-0 text-4xl sm:text-5xl bg-white text-gray-900"
+    )}>
       Footer
-    </div>
+    </footer>
   )
 }
