a60e474c4ca1efc8432f40380b822ef40e5621a4
DragonSenses <kendrickfrancisco@gmail.com>
2023-07-08 16:54:12 -0700

    Properly extract the props

--- app/Footer.js
+++ app/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-export default function Footer() {
+export default function Footer(props) {
+  const { dark } = props;
   return (
     <footer className={(dark ? 
       "flex items-center justify-center gap-10 py-4 px-0 text-4xl sm:text-5xl bg-gray-900 text-white" : 
